3ced4949a91e9d06c7670ce8b1ac5a1b22802d7e
Raymond Yegon <raymondyegon17@gmail.com>
2019-02-15 14:25:14 +0300

    Isosceles function identifier added

--- js/script.js
+++ js/script.js
@@ -6,13 +6,21 @@ function tracker() {
   var z;
     z = document.getElementById("num3").value;
   var array = [x, y, z];
-    if ( x + y < z || y + z < x || x + z < y ) {
-       alert("Cannot form a triangle")
+  if ( x + y < z || y + z < x || x + z < y ) {
+    output = "Cannot form a triangle";
+  }
+    if (x <= 0 || y<=0 || z<=0) {
+      output = "Not valid" ;
     }
-    if ( x == y && y==z ) {
-       alert ("Equilateral")
+     if ( x == y && y==z && x==z ) {
+       output = "Equilateral Triangle";
     }
-    if (x == y && y != z) {
-       alert ("Isosceles")
+     if ((x == y) && (y!=z) ) {
+       output = "Isosceles Triangle";
+     }
+     if ((x!=y) && (y!=z)) {
+       output = "Scalene Triangle";
+       }
     }
+  document.querySelector("#result").innerHTML = output;
 }
