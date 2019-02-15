f493afd3e396dbfe82df2a53cbe93ed2c0c710d4
Raymond Yegon <raymondyegon17@gmail.com>
2019-02-15 10:49:40 +0300

    Javascript file linked to index page

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function tracker() {
+  var x;
+  x = document.getElementById("num1").value;
+  var y ;
+  y = document.getElementById("num2").value;
+  var z;
+  z = document.getElementById("num3").value;
+}
