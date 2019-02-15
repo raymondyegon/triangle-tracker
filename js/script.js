d3b2adf37dfc7168a196c77e2434116944160113
Raymond Yegon <raymondyegon17@gmail.com>
2019-02-15 11:52:46 +0300

    javascript function to calculate if two added sides is greater than the third side added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 function tracker() {
   var x;
-  x = document.getElementById("num1").value;
+    x = document.getElementById("num1").value;
   var y ;
-  y = document.getElementById("num2").value;
+    y = document.getElementById("num2").value;
   var z;
-  z = document.getElementById("num3").value;
+    z = document.getElementById("num3").value;
+  var array = [x, y, z];
+    if ( x + y < z || y + z < x || x + z < y ) {
+       alert("Cannot form a triangle")
+    }
+    if ( x == y && y==z ) {
+       alert ("Equilateral")
+    }
+    if (x == y && )
 }
