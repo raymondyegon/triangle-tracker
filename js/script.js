76ebee92a2e0f67f569ed6fd85fb61c5b3bc34f7
Raymond Yegon <raymondyegon17@gmail.com>
2019-02-15 16:09:13 +0300

    Scalene equation added

--- js/script.js
+++ js/script.js
@@ -6,20 +6,19 @@ function tracker() {
   var z;
     z = document.getElementById("num3").value;
   var array = [x, y, z];
-  if ( x + y < z || y + z < x || x + z < y ) {
-    output = "Cannot form a triangle";
-  }
     if (x <= 0 || y<=0 || z<=0) {
       output = "Not valid" ;
+    }
+    if ( x + y >= z || y + z >= x || x + z >= y ) {
+       output = "Cannot form a triangle";
     }
      if ( x == y && y==z && x==z ) {
        output = "Equilateral Triangle";
     }
      if ((x == y) && (y!=z) ) {
        output = "Isosceles Triangle";
-     }
-     if ((x!=y) && (y!=z)) {
-       output = "Scalene Triangle";
+    if (x!=y && y!=z && x!=z) {
+       output = "Scalene Triangle"
        }
     }
   document.querySelector("#result").innerHTML = output;
