18de18209f776489ed8264ad48adfe9fea1b0a90
Raymond Yegon <raymondyegon17@gmail.com>
2019-02-16 18:00:17 +0300

    Zero value input added in javascript

--- js/script.js
+++ js/script.js
@@ -1,20 +1,17 @@
 function tracker() {
   var a;
-    a = document.getElementById("num1").value;
+    a = parseFloat(document.getElementById("num1").value);
   var b ;
-    b = document.getElementById("num2").value;
+    b = parseFloat(document.getElementById("num2").value);
   var c
-    c = document.getElementById("num3").value;
-  var array = [a, b, c];
-    if (a <= 0 || b<=0 || c<=0) {
-      output = "Not valid"
-    } else if ( ((a+ b) <=c) || ((b + c) <= a) || ((a + c) <= b) ) {
-       output = "Cannot form a triangle";
-    } else if ( a == b && b==c && a==c ) {
+    c = parseFloat(document.getElementById("num3").value);
+    if (a <= 0 || b<=0 || c<=0 || a+b <=c || b+c <=a || a+c <=b ) {
+      output = "Cannot form a triangle"
+    }  else if ( a == b && b==c && a==c ) {
        output = "Equilateral Triangle";
-    } else if ((a == b) || (b == c) || ( a == c )) {
+    } else if ((a === b) || (b === c) || ( a === c )) {
        output = "Isosceles Triangle";
-    } else if ( a!= b && b !=c && a != c) {
+    } else {
        output = "Scalene Triangle";
     }
   document.querySelector("#result").innerHTML = output;
